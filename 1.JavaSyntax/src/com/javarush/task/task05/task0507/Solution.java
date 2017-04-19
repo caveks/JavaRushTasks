b9fb27bc06ce2fd5a6e14b3767efc042c3c84951
caveks <caveks@gmail.com>
2017-04-19 22:13:20 +0300

    Solution

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,24 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int digit = Integer.parseInt(reader.readLine());
+        double x = 0;
+        int n = 1;
+
+        while (digit != -1){
+            x = x + digit;
+            digit = Integer.parseInt(reader.readLine());
+            if (digit != -1){
+                n++;
+            }
+        }
+        System.out.println(x / n);
     }
 }
 
