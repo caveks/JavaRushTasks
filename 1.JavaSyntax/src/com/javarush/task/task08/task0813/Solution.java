5b2d678b12cdb1b96229bc15a85d179878b8f6c1
caveks <caveks@gmail.com>
2017-05-22 23:28:36 +0300

    Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static HashSet<String> createSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("Л_1");
+        set.add("Л_2");
+        set.add("Л_3");
+        set.add("Л_4");
+        set.add("Л_5");
+        set.add("Л_6");
+        set.add("Л_7");
+        set.add("Л_8");
+        set.add("Л_9");
+        set.add("Л_10");
+        set.add("Л_11");
+        set.add("Л_12");
+        set.add("Л_13");
+        set.add("Л_14");
+        set.add("Л_15");
+        set.add("Л_16");
+        set.add("Л_17");
+        set.add("Л_18");
+        set.add("Л_19");
+        set.add("Л_20");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
