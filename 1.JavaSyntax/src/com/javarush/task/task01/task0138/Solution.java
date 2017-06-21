9f1850d64c5c01dc1914f6baa6575f69a9ea04e9
caveks <caveks@gmail.com>
2017-06-21 23:23:28 +0300

    Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println();
+        System.out.println("В лесу родилась елочка");
+        System.out.println("В лесу она росла,");
+        System.out.println("Зимой и летом стройная,");
+        System.out.println("Зеленая была.");
+    }
+}
