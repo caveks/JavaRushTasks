e82ebae005982b075db75976169f94b763aef08b
caveks <caveks@gmail.com>
2017-06-22 21:34:50 +0300

    Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0130/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0130;
+
+/* 
+Наш первый конвертер!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(40));
+    }
+
+    public static double convertCelsiumToFahrenheit(int celsium) {
+        double tf = celsium/((double)5/9) + 32;
+        return tf;
+    }
+}
