842f2daa78019eb3943d0cf553845db527465a40
caveks <caveks@gmail.com>
2017-08-08 23:47:30 +0300

    Solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for (int i = 0; i < array.length; i++){
+            String a = array[i];
+            for (int m = i + 1; m < array.length; m++){
+                String b = array[m];
+                if (isGreaterThan(a, b) == true){
+                    a = b;
+                    array[m] = array[i];
+                    array[i] = b;
+                }
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
