a7d36cb2f4317547aed7132058e48e5316109f56
caveks <caveks@gmail.com>
2017-08-14 19:15:33 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 6;
+        Integer y = new Integer(8);
+        print(x);
+        print(y);
+    }
+
+    public static void print(int a){
+
+    }
+
+    public static void print(Integer a){
+    }
+}
