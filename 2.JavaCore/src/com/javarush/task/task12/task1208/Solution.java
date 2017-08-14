ba98c57ba2cfb08c26d721b9d1c0def1e99ca69e
caveks <caveks@gmail.com>
2017-08-14 19:32:56 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(){
+
+    }
+
+    public static void print(String s){
+
+    }
+
+    public static void print(int i){
+
+    }
+
+    public static void print(long l){
+
+    }
+
+    public static void print(Integer integer){
+
+    }
+
+}
