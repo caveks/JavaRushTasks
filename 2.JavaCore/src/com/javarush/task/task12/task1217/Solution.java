ddfee6b5cde54691b241c4cb6df2c17a78814366
caveks <caveks@gmail.com>
2017-08-15 22:04:53 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        void startFly();
+    }
+
+    public interface Run {
+        void startRun();
+    }
+
+    public interface Swim {
+        void startSwim();
+    }
+
+}
