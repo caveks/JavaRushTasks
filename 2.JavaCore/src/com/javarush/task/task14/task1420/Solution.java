445595afa45c9ec255c8695fae185b3a34b39916
caveks <caveks@gmail.com>
2017-09-18 23:20:39 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task14.task1420;
+
+/* 
+НОД
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int a = Integer.parseInt(reader.readLine());
+                if(a <= 0) throw new NumberFormatException();
+            int b = Integer.parseInt(reader.readLine());
+                if(b <= 0) throw new NumberFormatException();
+            System.out.println(nod(a, b));
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+    }
+
+    public static int nod(int a, int b) {
+        if (b == 0)
+            return a;
+        return nod(b, a % b);
+    }
+}
