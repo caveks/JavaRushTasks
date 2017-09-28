c8c050e85559c0d08f40029996dd20390fa773a2
caveks <caveks@gmail.com>
2017-09-28 22:52:43 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements Flyable {
+
+    public int numPassengers;
+
+    public Plane(int numPassengers){
+        this.numPassengers = numPassengers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
