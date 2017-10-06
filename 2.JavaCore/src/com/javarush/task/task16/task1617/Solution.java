d760dc93edd9001d9221d29ec06d04c4907cd9d5
caveks <caveks@gmail.com>
2017-10-06 23:15:10 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int countSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (countSeconds > 0) {
+                    Thread.sleep(1000);
+                    System.out.print(countSeconds + " ");
+                    countSeconds--;
+                    if (countSeconds == 0) System.out.print("Марш!");
+                }
+            } catch (InterruptedException e) {
+                System.out.print(countSeconds + " Прервано!");
+            }
+        }
+    }
+}
