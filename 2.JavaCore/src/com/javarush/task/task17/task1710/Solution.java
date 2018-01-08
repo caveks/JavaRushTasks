f334f49e33a44aed86a094b54f7155deb30a0d28
caveks <caveks@gmail.com>
2018-01-08 23:12:51 +0300

    Solution

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task17.task1710;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
