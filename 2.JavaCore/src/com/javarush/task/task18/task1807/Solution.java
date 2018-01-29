ba23f4a6391c192dd333a9dfe10a3b08c9ef4948
caveks <caveks@gmail.com>
2018-01-29 21:19:10 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1807/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1807;
+
+/* 
+Подсчет запятых
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        int n = 0;
+        while (inputStream.available() > 0) {
+            int code = inputStream.read();
+            if (code == 44) {
+                n++;
+            }
+        }
+        System.out.println(n);
+        inputStream.close();
+    }
+}
