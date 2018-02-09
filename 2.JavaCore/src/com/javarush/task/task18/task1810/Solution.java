3c52071aea3db6ee10642ad548c286d49149f241
caveks <caveks@gmail.com>
2018-02-09 23:45:25 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task18.task1810;
+
+/* 
+DownloadException
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+        while (true) {
+            int n = 0;
+            while (inputStream.available() > 0) {
+                inputStream.read();
+                n++;
+            }
+            if (n < 1000) {
+                reader.close();
+                inputStream.close();
+                throw new DownloadException();
+            } else {
+                fileName = reader.readLine();
+                inputStream = new FileInputStream(fileName);
+            }
+        }
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
