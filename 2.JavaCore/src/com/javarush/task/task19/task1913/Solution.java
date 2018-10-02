30b74272b855dbf7b08ef12c603f3d243b1a9b66
caveks <caveks@gmail.com>
2018-10-02 21:26:08 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String s = outputStream.toString();
+        Pattern p = Pattern.compile("[0-9]");
+        Matcher m = p.matcher(s);
+        while (m.find()){
+            System.out.print(s.substring(m.start(),m.end()));
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
