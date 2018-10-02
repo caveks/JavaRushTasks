d71d1acad12572190f0838f504a5de1b068721aa
caveks <caveks@gmail.com>
2018-10-02 20:18:54 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String s = outputStream.toString();
+        String t = s.replace("te", "??");
+        System.out.println(t);
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
