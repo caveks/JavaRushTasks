6a3b47e0f96b74ef7c9920bf5cb7a970103e7dea
caveks <caveks@gmail.com>
2018-10-07 00:26:01 +0300

    Solution

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String s = outputStream.toString();
+        System.out.println(s);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        fileOutputStream.write(s.getBytes());
+        fileOutputStream.close();
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
