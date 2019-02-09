1b488cf1ab5c01646edaa852b8412f0e1879c07d
caveks <caveks@gmail.com>
2019-02-09 22:20:28 +0300

    Solution

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -4,7 +4,42 @@ package com.javarush.task.task19.task1920;
 Самый богатый
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReader = new FileReader(args[0]);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        TreeMap<String, Double> list = new TreeMap<>();
+        double max = 0;
+
+        while (bufferedReader.ready()){
+            String[] s = bufferedReader.readLine().split(" ");
+            if (list.containsKey(s[0])) {
+                list.put(s[0], list.get(s[0]) + Double.parseDouble(s[1]));
+                if (list.get(s[0]) > max){
+                    max = list.get(s[0]);
+                }
+            } else {
+                list.put(s[0], Double.parseDouble(s[1]));
+                if (list.get(s[0]) > max){
+                    max = list.get(s[0]);
+                }
+            }
+         }
+        for (Map.Entry<String, Double> entry : list.entrySet()) {
+            String surname = entry.getKey();
+            double d = entry.getValue();
+            if (d == max){
+                System.out.println(surname);
+            }
+        }
+        fileReader.close();
     }
 }
